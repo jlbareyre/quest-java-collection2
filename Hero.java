ebfa796bcd613a101bf481912c8cb411bd9b70e0
Jean-Loup BAREYRE <jlbareyre@gmail.com>
2020-05-23 23:59:37 +0200

    Quest 610

--- Hero.java
+++ Hero.java
@@ -1,4 +1,6 @@
-public class Hero {
+import java.util.Comparator;
+
+public class Hero implements Comparable<Hero>{
 
     // attributs
     private String name;
@@ -26,5 +28,30 @@ public class Hero {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public int compareTo(Hero hero1) {
+        // TODO Auto-generated method stub
+        return this.getName().compareTo(hero1.getName());
+    }
+
+}
+
+class SortByNameDesc implements Comparator<Hero> {
+    public int compare(Hero hero1, Hero hero2){
+        return hero2.getName().compareTo(hero1.getName());
+    }
+}
+
+class SortByAge implements Comparator<Hero> {
+    public int compare(Hero hero1, Hero hero2) {
+        return hero1.getAge() - hero2.getAge();
+    }
+}
+
+class SortByAgeDesc implements Comparator<Hero> {
+    public int compare(Hero hero1, Hero hero2) {
+        return hero2.getAge() - hero1.getAge();
     }
 }
